e26816e92ee4da37d42090f1431249c1c3dfe58c
TrueJacobG <jakubgradzewicz1309@gmail.com>
2022-03-26 12:10:41 +0100

    new item format, item buttons are separated from items

--- src/main/java/game/components/Item.java
+++ src/main/java/game/components/Item.java
@@ -1,14 +1,6 @@
 package game.components;
 
-import screen.constants.Size;
-
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class Item extends JButton implements ActionListener {
+public class Item{
     private String id;
     private String name;
     private String durability;
@@ -22,46 +14,15 @@ public class Item extends JButton implements ActionListener {
         this.durability = durability;
         this.type = type;
         this.info = info;
-        // from id get name etc
+    }
 
-        this.setText(this.name);
-
-        // pos to the image, for ex. top means above icon
-        this.setHorizontalTextPosition(JButton.CENTER);
-        this.setVerticalTextPosition(JButton.TOP);
-
-        this.setOpaque(true);
-
-        this.setFont(new Font("Arial", Font.PLAIN,20));
-
-        this.setVerticalAlignment(JButton.CENTER);
-        this.setHorizontalAlignment(JButton.CENTER);
-        this.setBounds(0, 0, Size.box_size, Size.box_size);
-
-        this.setForeground(Color.BLACK);
-
-        Border border;
-        if(name.equals("")){
-            border = BorderFactory.createLineBorder(Color.WHITE, 5);
-            this.setBackground(Color.WHITE);
-        } else {
-            border = BorderFactory.createLineBorder(Color.GRAY, 5);
-            this.setBackground(Color.LIGHT_GRAY);
-        }
-
-        this.setBorder(border);
-
-        this.addActionListener(this);
+    public String getName(){
+        return this.name;
     }
 
     public String toString(){
         return this.name;
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == this) {
-            System.out.println(this.name);
-        }
-    }
+
 }
---
+++ src/main/java/screen/buttons/ItemButton.java
@@ -0,0 +1,49 @@
+package screen.buttons;
+
+import game.components.Item;
+import screen.constants.Size;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ItemButton extends JButton implements ActionListener{
+
+    private Item item;
+
+    public ItemButton(Item item){
+        this.item = item;
+
+        this.setFocusable(false);
+        this.setText(item.getName());
+
+        this.setHorizontalTextPosition(JButton.CENTER);
+        this.setVerticalTextPosition(JButton.TOP);
+
+        this.setOpaque(true);
+
+        this.setFont(new Font("Arial", Font.PLAIN,20));
+
+        this.setVerticalAlignment(JButton.CENTER);
+        this.setHorizontalAlignment(JButton.CENTER);
+        this.setPreferredSize(new Dimension(Size.boxWidth, Size.boxHeight));
+
+        this.setForeground(Color.BLACK);
+        this.setBackground(Color.WHITE);
+
+        Border border = BorderFactory.createLineBorder(Color.GRAY, 3);
+        this.setBorder(border);
+
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this) {
+            System.out.println(this.item.getName());
+        }
+    }
+}
+
