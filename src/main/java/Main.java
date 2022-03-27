5bfba42c49698d6e7e1eca4731f449a83c5551fb
TrueJacobG <jakubgradzewicz1309@gmail.com>
2022-03-27 13:38:39 +0200

    spells init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import game.Game;
-import game.components.Equipment;
-import game.components.Item;
 
 public class Main {
     public static void main(String[] args){
--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,6 +1,7 @@
 package game;
 
 import game.components.Equipment;
+import game.components.Spells;
 import screen.GameScreen;
 
 import javax.swing.*;
@@ -8,6 +9,7 @@ import javax.swing.*;
 public class Game {
     public static void run(){
         Equipment.init();
+        Spells.init();
         JFrame gameScreen = new GameScreen();
     }
 }
