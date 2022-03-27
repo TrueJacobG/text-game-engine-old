d9f95a30c2c1107045a0526fd1eeda0382841018
TrueJacobG <jakubgradzewicz1309@gmail.com>
2022-03-27 14:58:20 +0200

    new layout of left panel

---
+++ src/main/java/screen/panels/LeftPanel.java
@@ -0,0 +1,12 @@
+package screen.panels;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LeftPanel extends JPanel {
+    public LeftPanel(){
+        this.setBackground(Color.YELLOW);
+
+    }
+}
